e8598b584e58bde75f97e3a7a787f90abf8b189f
tourn <latzer.daniel@gmail.com>
2015-11-12 13:41:33 +0100

    Rename things in TrackLearner for clarity

--- src/main/java/ch/trq/carrera/javapilot/akka/TrackLearner.java
+++ src/main/java/ch/trq/carrera/javapilot/akka/TrackLearner.java
@@ -4,42 +4,39 @@ import akka.actor.ActorRef;
 import akka.actor.Props;
 import akka.actor.UntypedActor;
 //import com.sun.tools.internal.jxc.ap.Const;
-import ch.trq.carrera.javapilot.akka.trackanalyzer.Round;
 import ch.trq.carrera.javapilot.akka.trackanalyzer.State;
 import ch.trq.carrera.javapilot.akka.trackanalyzer.Track;
 import ch.trq.carrera.javapilot.math.PhysicModel;
 import ch.trq.carrera.javapilot.math.PhysicModelCalculator;
 import com.zuehlke.carrera.javapilot.akka.PowerAction;
-import com.zuehlke.carrera.javapilot.akka.experimental.ThresholdConfiguration;
 import ch.trq.carrera.javapilot.akka.trackanalyzer.TrackAnalyzer;
-import com.zuehlke.carrera.relayapi.messages.RoundTimeMessage;
 import com.zuehlke.carrera.relayapi.messages.SensorEvent;
 import com.zuehlke.carrera.relayapi.messages.VelocityMessage;
 import com.zuehlke.carrera.timeseries.FloatingHistory;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
-import visualization.DataChart;
 
 /**
- *  A very simple actor that determines the power value by a configurable Threshold on any of the 10 observables
+ *  An actor driving with constant power, learning the structure of the track
  */
 public class TrackLearner extends UntypedActor {
+
+    private static final int MOVE_START_GYROZ_THRESHOLD = 10;
+    private static final int MOVE_START_POWER_INCREASE = 10;
+    private static final double TURN_THRESHOLD = 800;
+    private static final int MOVE_TRY_POWER_INCREASE = 1;
 
     private ActorRef pilot;
     private FloatingHistory gyroZ;
     private TrackAnalyzer trackAnalyzer;
     private PhysicModel physicModel;
     private PhysicModelCalculator physicModelCalculator;
-    private State state = State.STRAIGHT;
-    private static double TURN_THRESHOLD = 800;
+    private State turnState = State.STRAIGHT;
     private final Logger LOGGER = LoggerFactory.getLogger(TrackLearner.class);
 
     private int power;
-    private int increasePowerRate = 1;
-    private int amountOverMovePower =10;
-    private int powerIncreaseForPhysicCalculation = 20;
 
-    private boolean hasStartedMoving = false;
+    private boolean isMoving = false;
     private boolean trackRecognitionFinished = false;
 
     public TrackLearner(ActorRef pilot, int startPower, int startRoundNr, int amountOfRounds, int faultyGoingStraightTime, int faultyTurnTime, int floatingHistorySize) {
@@ -58,7 +55,6 @@ public class TrackLearner extends UntypedActor {
                 track.setPower(power);
                 physicModelCalculator = new PhysicModelCalculator(track,physicModel);
                 physicModelCalculator.calculateTrackPhysics();
-                power += powerIncreaseForPhysicCalculation;
                 LOGGER.info("Track Received");
                 //pilot.tell(track, ActorRef.noSender());
             }
@@ -93,10 +89,10 @@ public class TrackLearner extends UntypedActor {
     private void handleSensorEvent(SensorEvent event) {
         gyroZ.shift(event.getG()[2]);
         if(!trackRecognitionFinished){
-            if(!hasStartedMoving){
-                carHasNotStarted();
+            if(!isMoving){
+                tryStartMoving();
             }else{
-                switch (state) {
+                switch (turnState) {
                     case STRAIGHT:
                         straightAction(event.getTimeStamp());
                         break;
@@ -114,31 +110,32 @@ public class TrackLearner extends UntypedActor {
 
     private void straightAction(long timeStamp) {
         if (gyroZ.currentMean() > TURN_THRESHOLD) {
-            state = State.RIGHT;
-            trackAnalyzer.addTrackSection(state,timeStamp);
+            turnState = State.RIGHT;
+            trackAnalyzer.addTrackSection(turnState,timeStamp);
             //LOGGER.info("RIGHT TURN");
         }else if(-gyroZ.currentMean() > TURN_THRESHOLD){
-            state = State.LEFT;
-            trackAnalyzer.addTrackSection(state,timeStamp);
+            turnState = State.LEFT;
+            trackAnalyzer.addTrackSection(turnState,timeStamp);
             //LOGGER.info("LEFT TURN");
         }
     }
 
     private void turnAction(long timeStamp) {
         if (Math.abs(gyroZ.currentMean()) < TURN_THRESHOLD) {
-            state = State.STRAIGHT;
-            trackAnalyzer.addTrackSection(state,timeStamp);
+            turnState = State.STRAIGHT;
+            trackAnalyzer.addTrackSection(turnState,timeStamp);
             //LOGGER.info("GOING STRAIGHT");
         }
     }
 
-    private void carHasNotStarted() {
-        hasStartedMoving = gyroZ.currentMean() > 10;
-        if(hasStartedMoving){
+    private void tryStartMoving() {
+        isMoving = gyroZ.currentMean() > MOVE_START_GYROZ_THRESHOLD;
+
+        if(isMoving){
             physicModel.setStartPower(power);
-            power += amountOverMovePower;
+            power += MOVE_START_POWER_INCREASE;
         }else{
-            power += increasePowerRate;
+            power += MOVE_TRY_POWER_INCREASE;
         }
         //LOGGER.info("MY POWER: " + power);
     }
